52158b681d90017c17651271bcad1187913a16b5
njprogramming <nicholasjacksonprogramming@gmail.com>
2013-10-05 23:22:11 +1000

    Initial Commit

---
+++ bmp2raw/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  bmp2raw
+//
+//  Created by nicholas jackson on 5/10/13.
+//  Copyright (c) 2013 nicholas jackson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
